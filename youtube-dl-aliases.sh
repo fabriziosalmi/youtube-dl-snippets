4d7a8b6db6ebd42f49797527a4f4aab85fc1973b
fab <fabrizio.salmi@gmail.com>
2017-10-08 15:18:46 +0200

    Update youtube-dl-aliases.sh

--- youtube-dl-aliases.sh
+++ youtube-dl-aliases.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 alias youtube-dl-mp3='youtube-dl --extract-audio --audio-format mp3'
 alias youtube-dl-playlist-mp3='youtube-dl --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s"'
-echo "youtube-dl-aliases installed:\nyoutube-dl-mp3 YOUTUBE_VIDEO_URL Download and convert YouTube video to mp3 file\nyoutube-dl-playlist-mp3 YOUTUBE_PLAYLIST_URL Download and convert a YouTube playlist to several mp3 files"
+echo "${GREEN}https://github.com/fabriziosalmi/youtube-dl-snippets\n\n${RED}youtube-dl-mp3 YOUTUBE_VIDEO_URL${NC} Download and convert YouTube video to mp3 file\n${RED}youtube-dl-playlist-mp3 YOUTUBE_PLAYLIST_URL${NC} Download and convert a YouTube playlist to several mp3 files\n"
